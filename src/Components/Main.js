0f2e58b8d31234239788ee4907f685e401f86d3a
mnapoleon <L5hiv9P8jL1G>
2018-03-05 13:55:16 -0500

    finished sec 7 lecture 49

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,38 +2,28 @@ import React, {Component} from 'react'
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
-import {Route} from 'react-router-dom'
-import {removePost} from '../redux/actions'
+import {Route, Link} from 'react-router-dom'
 
 class Main extends Component {
-    constructor() {
-        super()
-    }
-
-    componentDidMount() {
-      this.props.removePost(1)
-    }
-
     render() {
-        return (
-
+      console.log(this.props)
+      return (
         <div>
-            <Route exact path = "/" render={() => (
-                 <div>
-                      <Title title={'Photowall'}/>
-                      <PhotoWall {...this.props}/>
-                 </div>
+          <h1>
+            <Link to="/"> Photowall </Link>
+          </h1>
+          <Route exact path = "/" render={() => (
+               <div>
+                    <PhotoWall {...this.props}/>
+               </div>
 
-            )}/>
+          )}/>
 
-            {/*<Route path= "/AddPhoto" render = {({history}) => (
-                <AddPhoto onAddPhoto={(addedPost) => {
-                    this.addPhoto(addedPost)
-                    history.push('/')
-                }}/>
-            )}/> */}
-         </div>
-        )
+          <Route path= "/AddPhoto" render = {({history}) => (
+              <AddPhoto {...this.props} onHistory={history} />
+          )}/>
+       </div>
+      )
     }
 
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,17 @@
 import posts from '../data/posts'
 
 const postReducer = function posts(state = posts, action) {
-  console.log(action.index)
-  return state
+  console.log(action.type)
+
+  switch (action.type) {
+    case 'REMOVE_POST': return [
+      ...state.slice(0, action.index),
+      ...state.slice(action.index + 1)
+    ]
+    case 'ADD_POST' : return [...state, action.post]
+    default:
+      return state
+  }
 }
 
 export default postReducer
