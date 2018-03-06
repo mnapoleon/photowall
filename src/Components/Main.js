5468072132f415778bf0d4d0125fe01c8095c6b1
mnapoleon <L5hiv9P8jL1G>
2018-03-06 11:25:59 -0500

    finished sec 8 lecture 54

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import Title from './Title'
 import PhotoWall from './PhotoWall'
 import AddPhoto from './AddPhoto'
 import {Route, Link} from 'react-router-dom'
+import Single from './Single'
 
 class Main extends Component {
     render() {
@@ -21,6 +22,10 @@ class Main extends Component {
 
           <Route path= "/AddPhoto" render = {({history}) => (
               <AddPhoto {...this.props} />
+          )}/>
+
+          <Route path="/single/:id" render = {(params) => (
+            <Single {...this.props} {...params}/>
           )}/>
        </div>
       )
